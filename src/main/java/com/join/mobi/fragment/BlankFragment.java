7c3ddc82794201c6c44ca2d74e43f0777e6ffb13
lala-mac <mwj7999@hotmail.com>
2014-09-22 00:35:27 +0800

    a

---
+++ src/main/java/com/join/mobi/fragment/BlankFragment.java
@@ -0,0 +1,21 @@
+package com.join.mobi.fragment;
+
+import android.support.v4.app.Fragment;
+import com.join.android.app.common.R;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * User: mawanjin@join-cn.com
+ * Date: 14-9-21
+ * Time: 下午11:15
+ */
+@EFragment(R.layout.blank_fragment_layout)
+public class BlankFragment extends Fragment {
+
+    @AfterViews
+    void afterViews() {
+
+    }
+
+}
